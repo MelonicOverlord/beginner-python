31f289c5b72cc11454f56d70bb7454e30f274614
BatemaDevelopment <lukasbatema@gmail.com>
2022-12-26 16:37:16 +0000

    Fix Melonic's horrible python

---
+++ beginner_python/__main__.py
@@ -0,0 +1,15 @@
+from commenting_out import commenting_out
+from print_statements import print_statements
+from variables import variables
+from inputting_numbers_to_variables import inputting_numbers_to_variables
+from if_elif_else_statements import if_elif_else_statements
+
+def main():
+    commenting_out()
+    print_statements()
+    variables()
+    inputting_numbers_to_variables()
+    if_elif_else_statements()
+
+if __name__ == "__main__":
+    main()
