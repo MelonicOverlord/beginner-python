8af8184fbb057e9fcd422e7b4269b9aa3b205a02
BatemaDevelopment <lukasbatema@gmail.com>
2023-01-14 07:50:34 -0800

    Update __main__.py

--- beginner_python/__main__.py
+++ beginner_python/__main__.py
@@ -1,8 +1,8 @@
-from commenting_out import commenting_out
-from print_statements import print_statements
-from variables import variables
-from inputting_numbers_to_variables import inputting_numbers_to_variables
-from if_elif_else_statements import if_elif_else_statements
+from commenting_out_lesson_one import commenting_out
+from print_statements_lesson_two import print_statements
+from variables_lesson_three import variables
+from inputting_numbers_to_variables_lesson_four import inputting_numbers_to_variables
+from if_elif_else_statements_lesson_five import if_elif_else_statements
 
 def main():
     commenting_out()
